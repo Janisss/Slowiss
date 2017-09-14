aa590eb88492eccede3b5aea5f910ff17702b8cf
soltesyzak <30795151+soltesyzak@users.noreply.github.com>
2017-09-14 15:22:26 +0200

    scroll

--- scrollTop.php
+++ scrollTop.php
@@ -1,6 +1,8 @@
 <!DOCTYPE html>
 <style type="text/css">
 	#myBtn {
+	width: 60px;
+	height: 60px;	
     display: none; /* Hidden by default */
     position: fixed; /* Fixed/sticky position */
     bottom: 0px; /* Place the button at the bottom of the page */
@@ -11,13 +13,21 @@
     background-color: #9d0b0f; /* Set a background color */
     color: white; /* Text color */
     cursor: pointer; /* Add a mouse pointer on hover */
-    padding: 9px; /* Some padding */
+	padding: 9px;
+	font-size: 15px;
     border-radius: 1px; /* Rounded corners */
+	-webkit-transition: width 1s, height 1s, font-size 1s, -webkit-transform 1s; /* Safari */
+    transition: width 1s, height 1s, font-size 1s, transform 1s;
 }
 
-#myBtn:hover {
-    background-color: #555; /* Add a dark-grey background on hover */
-}
+	#myBtn:hover{
+	width: 80px;
+	height: 80px;
+	font-size: 20px;
+	-webkit-transform: rotate(360deg); /* Safari */
+    transform: rotate(360deg);
+	content: "top";
+	}
 </style>
 
 <script type="text/javascript">
@@ -38,5 +48,7 @@
 	    document.documentElement.scrollTop = 0; // For IE and Firefox
 	}
 </script>
-
-<button onclick="topFunction()" id="myBtn" title="Go to top">Top</button>
+<div style="text-align: center;">
+<button onclick="topFunction()" id="myBtn" title="Go to top"><i class="fa fa-arrow-up" aria-hidden="true"></i>
+</button>
+</div>
